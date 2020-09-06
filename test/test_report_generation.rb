4917eef6a99bc511077a539f6d2c6d3965aeb1b6
Balabharathi Jayaraman <balabharathi.jayaraman@gmail.com>
2020-09-06 11:18:00 +0200

    unit test refactoring

--- test/test_report_generation.rb
+++ test/test_report_generation.rb
@@ -3,25 +3,24 @@ require "./lib/nxgreport.rb"
 
 class TestNxgReportGeneration < Test::Unit::TestCase
 
-    def setup
+    def setup()
         @default_report_location = "./NxgReport.html"
         File.delete(@default_report_location) if File.file?(@default_report_location)
+        @nxg_report = NxgReport.new()
     end
 
     def test_report_did_not_generate_when_no_tests_logged()
-        nxg_report = NxgReport.new()
-        nxg_report.setup()
-        report_location = nxg_report.report_location()
-        nxg_report.build()
+        @nxg_report.setup()
+        report_location = @nxg_report.report_location()
+        @nxg_report.build()
         assert(!File.file?(report_location))
     end
 
     def test_report_is_created_when_one_test_is_logged()
-        nxg_report = NxgReport.new()
-        nxg_report.setup()
-        report_location = nxg_report.report_location()
-        nxg_report.log_test("Feature One", "Pass")
-        nxg_report.build()
+        @nxg_report.setup()
+        report_location = @nxg_report.report_location()
+        @nxg_report.log_test("Feature One", "Pass")
+        @nxg_report.build()
         assert(File.file?(report_location))
     end
 
--- test/test_setup_methods.rb
+++ test/test_setup_methods.rb
@@ -3,15 +3,15 @@ require "./lib/nxgreport.rb"
 
 class TestNxgReport < Test::Unit::TestCase
     
-    def setup
+    def setup()
         @expected_default_report_location = "./NxgReport.html"
         File.delete(@expected_default_report_location) if File.file?(@expected_default_report_location)
+        @nxg_report = NxgReport.new()
     end
 
     def test_default_location()
-        nxg_report = NxgReport.new()
-        nxg_report.setup()
-        assert_equal(@expected_default_report_location, nxg_report.report_location())
+        @nxg_report.setup()
+        assert_equal(@expected_default_report_location, @nxg_report.report_location())
     end
 
 end
