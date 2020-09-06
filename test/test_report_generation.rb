df3f87a57f217fba3ff4bd53de0af59114d53b66
Balabharathi Jayaraman <balabharathi.jayaraman@gmail.com>
2020-09-06 11:16:14 +0200

    unit test refactoring

--- test/test_report_generation.rb
+++ test/test_report_generation.rb
@@ -1,17 +1,12 @@
 require "test/unit"
 require "./lib/nxgreport.rb"
 
-class TestNxgReport < Test::Unit::TestCase
-    
-    def test_default_location()
-        nxg_report = NxgReport.new()
-        nxg_report.setup()
-        assert_equal("./NxgReport.html", nxg_report.report_location())
-    end
-
-end
-
 class TestNxgReportGeneration < Test::Unit::TestCase
+
+    def setup
+        @default_report_location = "./NxgReport.html"
+        File.delete(@default_report_location) if File.file?(@default_report_location)
+    end
 
     def test_report_did_not_generate_when_no_tests_logged()
         nxg_report = NxgReport.new()
---
+++ test/test_setup_methods.rb
@@ -0,0 +1,17 @@
+require "test/unit"
+require "./lib/nxgreport.rb"
+
+class TestNxgReport < Test::Unit::TestCase
+    
+    def setup
+        @expected_default_report_location = "./NxgReport.html"
+        File.delete(@expected_default_report_location) if File.file?(@expected_default_report_location)
+    end
+
+    def test_default_location()
+        nxg_report = NxgReport.new()
+        nxg_report.setup()
+        assert_equal(@expected_default_report_location, nxg_report.report_location())
+    end
+
+end
